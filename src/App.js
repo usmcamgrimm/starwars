a03162dc5c579ead27beb1f3d974f8457c14c5c2
Adrian Grimm <usmcamgrimm@gmail.com>
2022-04-23 14:27:53 -0400

    Adding some content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import logo from './images/sw-logo.png'
 
 function App() {
   return(
-    <div>
-      <p>
-        Star Wars
-      </p>
+    <div className="bg-[url('./images/star-lines.jpg')] w-full h-screen bg-cover bg-center">
+      <div className="container">
+        <div className='flex items-center justify-center hscreen pt-6'>
+          <img src={logo} alt='Star Wars logo' className='lg:w-3/5 md:w-full' />
+        </div>
+        <div className="container">
+          <div className="flex items-center justify-center hscreen mt-12">
+            <p>Characters</p>
+            <p>Planets</p>
+            <p>Species</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
