48cf79cc1b677f97ed29731cc772ecb4850fcd4d
Adrian Grimm <usmcamgrimm@gmail.com>
2022-04-28 12:04:56 -0400

    Add lightsaber colors to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     container: {
       center: true
     },
+    colors: {
+      'blue': '#2e67f8', // rgb(46, 103, 248)
+      'red': '#eb212e', // rgb(235, 33, 46)
+      'green': '#2ff924', // rgb(47, 249, 36)
+    },
   },
   plugins: [],
 }
