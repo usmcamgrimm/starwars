b8352c86e0bd71b187a14ae51379f825a603cde8
Adrian Grimm <usmcamgrimm@gmail.com>
2022-05-04 06:18:13 -0400

    Adding the table back in as a reference point

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from '../header/Header'
 import CharacterCard from '../character/CharacterCard'
+import Character from '../character/Character'
 import '@fontsource/inter'
 import '@fontsource/orbitron'
 import './app.css'
@@ -24,6 +25,7 @@ useEffect(() => {
   return(
     <div className="container">
       <Header />
+      <Character characters={characters} />
       <CharacterCard characters={characters} />
     </div>
   )
