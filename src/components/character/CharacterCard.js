2710d756ed7b7b3942a79c867f1fbe0dd360ed6c
Adrian Grimm <usmcamgrimm@gmail.com>
2022-05-06 11:55:39 -0400

    Still working on a card layout

--- src/components/character/CharacterCard.js
+++ src/components/character/CharacterCard.js
@@ -1,45 +1,42 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 import './charactercard.css'
 
-function CharacterCard(character) {
-  const [cards, setCards] = useState([])
-  const getCardLayout = () => {
-    const characterCards = cards.map((character, index) => {
-      return (
-        <Col>
-          <Card style={{ width: '16rem' }}>
-            <Card.Body>
+function CharacterCard(characters) {
+  const cardsPerRow = 5
+
+  const getCardLayout = (props, characters) => {
+    return (
+      <Col>
+        <Card style={{ width: '16rem' }}>
+          <Card.Body>
+          {props.characters.map((character, index) => (
+            <div>
               <Card.Title key={index}>
-                {character.name}
+              {character.name}
               </Card.Title>
               <Card.Text>{character.homeworld}</Card.Text>
               <Card.Text>{character.species}</Card.Text>
               <Card.Text>{character.birth_year}</Card.Text>
               <Card.Text>{character.height}</Card.Text>
               <Card.Text>{character.mass}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      )
-    })
-    return characterCards
+            </div>
+          ))}
+          </Card.Body>
+        </Card>
+      </Col>
+    )
   }
   
   return (
-    <Card style={{ width: '16rem' }}>
-      <Card.Body>
-        <Card.Title>
-          {character.name}
-        </Card.Title>
-        <Card.Text>{character.homeworld}</Card.Text>
-        <Card.Text>{character.species}</Card.Text>
-        <Card.Text>{character.birth_year}</Card.Text>
-        <Card.Text>{character.height}</Card.Text>
-        <Card.Text>{character.mass}</Card.Text>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row xs={1} md={cardsPerRow}>
+        {getCardLayout()}
+      </Row>
+    </Container>
   )
 }
 
