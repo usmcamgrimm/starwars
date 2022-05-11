f630e4bf4289271249d3d005b7a53635f9450a96
Adrian Grimm <usmcamgrimm@gmail.com>
2022-05-11 18:45:43 -0400

    I didn't think would work

--- src/components/Character/CharacterCard.js
+++ src/components/Character/CharacterCard.js
@@ -5,21 +5,88 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import './character-card.css'
 
+export const charFetch = async url => {
+  const res = await fetch(url)
+
+  if (!res.ok) {
+    throw Error("Error fetching data")
+  }
+
+  const data = await res.json()
+  return data.results
+}
+
 function CharacterCard() {
   const [characters, setCharacters] = useState([])
   // const [loading, setLoading] = useState(true);
-  const cardsPerRow = 5
+  const cardsPerRow = 2
 
   useEffect(() => {
-    async function fetchCharacters() {
-      const res = await fetch('https://swapi.dev/api/people/')
-      const data = await res.json()
-      setCharacters(data.results)
-      console.log(data.results)
+    const fetchCharacters = async () => {
+      const url = 'https://swapi.dev/api/people/'
+      const charList = await charFetch(url)
+      const fetchPlanets = await resolvePlanets(charList.results)
+      const fetchSpecies = await resolveSpecies(fetchPlanets)
+
+      return fetchSpecies
     }
 
-    fetchCharacters()
+    const resolvePlanets = async charactersList => {
+      const rawCharacterList = await charactersList.map(async character => {
+        const homeworld = await charFetch(character.homeworld)
+        const { name } = homeworld
+
+        return {
+          ...character,
+          homeworld: name
+        }
+      })
+      return Promise.all(rawCharacterList)
+    }
+
+    const resolveSpecies = async charactersList => {
+      const rawCharacterList = await charactersList.map(async character => {
+        if (character.species.length > 0) {
+          const species = await charFetch(character.species[0])
+          const { name, homeworld } = species
+
+          return {
+            ...character,
+            species: name,
+            name,
+            homeworld
+          }
+        } else {
+          return {
+            ...character,
+            species: 'Unknown Origin'
+          }
+        }
+      })
+      return Promise.all(rawCharacterList)
+    }
+
+    const fetchPlanets = async () => {
+      const url = 'https://swapi.dev/api/planets/'
+      const planetList = await charFetch(url)
+      const showPlanets = await resolvePlanets(planetList.results)
+
+      return showPlanets
+    }
+
+    console.error()
   }, [])
+
+  // useEffect(() => {
+  //   async function fetchCharacters() {
+  //     const res = await fetch('https://swapi.dev/api/people/')
+  //     const data = await res.json()
+  //     setCharacters(data.results)
+  //     console.log(data.results)
+  //   }
+
+  //   fetchCharacters()
+  // }, [])
 
   const setCardLayout = () => {
     let charCards = characters.map((character, index) => {
