72fc793df8271d1024a1831a44bdebffe82d8922
Adrian Grimm <usmcamgrimm@gmail.com>
2022-06-08 18:35:32 -0400

    Maybe I'll work with this later as a component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,20 +1,30 @@
-function Pagination({ charactersPerPage, totalCharacters, paginate }) {
+// import { Pagination } from 'react-bootstrap'
+
+function CardPagination({ charactersPerPage, totalCharacters, paginate }) {
     const pageNumbers = []
 
     for (let i = 1; i <= Math.ceil(totalCharacters / charactersPerPage); i++) {
         pageNumbers.push(i)
     }
+
     return (
-      <ul className="pagination">
-        {pageNumbers.map(number => (
-          <li key={number} className="page-item">
-            <a onClick={() => paginate(number)} href="!#" className="page-link">
-              {number}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <nav>
+        <ul className="pagination">
+          {pageNumbers.map(number => (
+            <li key={number} className="page-item">
+              <a onClick={() => paginate(number)} href="!#" className="page-link">
+                {number}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      // <nav>
+      //   <Pagination>
+      //     {pageNumbers}
+      //   </Pagination>
+      // </nav>
     )
 }
 
-export default Pagination
+export default CardPagination
